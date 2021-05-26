c6467912c17567f85a63d366bb84b147a27a88b7
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-05-26 10:44:34 +0100

    chore: refactor png icons

---
+++ assets/images/index.js
@@ -0,0 +1,10 @@
+export const Vegetable = require("./Vegetable.png");
+export const Fruits = require("./Fruits.png");
+export const Bread = require("./Bread.png");
+export const Sweets = require("./Sweets.png");
+export const Pasta = require("./Pasta.png");
+export const Drink = require("./Drink.png");
+export const Flower = require("./Flower.png");
+export const Lettuce = require("./Lettuce.png");
+export const Cabbage = require("./Cabbage.png");
+export const Caurliflower = require("./Caurliflower.png");

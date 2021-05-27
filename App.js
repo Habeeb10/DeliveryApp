b2b9f7e5f9936389aa5be2f6b53982f8bf7963d4
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-05-27 10:40:10 +0100

    chore:intall App packages

--- App.js
+++ App.js
@@ -2,11 +2,16 @@ import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import MainNavigator from "./src/Navigator/MainNavigator";
+import TabNavigator from "./src/Navigator/TabNavigator";
+import Navigator from "./src/Navigator/MainNavigator";
+import MyTabs from "./src/Navigator/TabNavigator";
+
 export default function App() {
   return (
     <NavigationContainer>
-      <MainNavigator/>
-    </NavigationContainer>
+    <MyTabs/>
+    </NavigationContainer>    
+    
   );
 }
 

532c620c4c581f914ef17955bd02ca36097221a0
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-05-27 10:37:53 +0100

    feat: implemented tab navigator

--- src/Navigator/MainNavigator.js
+++ src/Navigator/MainNavigator.js
@@ -1,16 +1,22 @@
-import * as React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import Categories from "../screens/Categories";
-import Vegetable from "../screens/Vegetable";
-const Stack = createStackNavigator();
+// import * as React from "react";
+// import { createStackNavigator } from "@react-navigation/stack";
+// import Categories from "../screens/Categories";
+// import { MyTabs } from "../Navigator/TabNavigator";
+// import Vegetable from "../screens/Vegetable";
+// import Items from "../screens/Item";
+// import Checkout from "../screens/Checkout";
+// import Payment from "../screens/Payment";
+// const Stack = createStackNavigator();
 
-export default function Navigator() {
-  return (
-    <Stack.Navigator headerMode={false}>
-      <Stack.Screen name="categories" component={Categories} />
-      <Stack.Screen name="vegetable" component={Vegetable} />
+// export default function Navigator() {
+//   return (
+//     <Stack.Navigator headerMode={false}>
+//       <Stack.Screen name="categories" component={Categories} />
+//       <Stack.Screen name="vegetable" component={Vegetable} />
+//       <Stack.Screen name="items" component={Items} />
+//       <Stack.Screen name="checkout" component={Checkout} />
+//       <Stack.Screen name="payment" component={Payment} />
       
-      
-    </Stack.Navigator>
-  );
-}
+//     </Stack.Navigator>
+//   );
+// }

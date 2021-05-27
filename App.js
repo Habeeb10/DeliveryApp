6ff4e9785da642c9c9fbf61f611eab8859350387
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-05-27 11:43:47 +0100

    Revert "Dev"

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-import "react-native-gesture-handler";
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import MainNavigator from "./src/Navigator/MainNavigator";
-import TabNavigator from "./src/Navigator/TabNavigator";
-import Navigator from "./src/Navigator/MainNavigator";
-import MyTabs from "./src/Navigator/TabNavigator";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-    <MyTabs/>
-    </NavigationContainer>    
-    
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
